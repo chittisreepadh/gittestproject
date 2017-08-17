99c2b1ae17e7a3307ce37fa25e38af2c8d8f6435
bandaru <root@was.eidikointernal.com>
2017-08-18 00:20:46 +0530

    change

---
+++ gittestproject1/src/main/java/com/eidiko/gittestproject1/App.java
@@ -0,0 +1,13 @@
+package com.eidiko.gittestproject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World25!" );
+    }
+}
